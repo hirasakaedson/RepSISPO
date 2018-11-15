8d49383e72f08060a37daf186df28d8ad3e89cad
Ariel Veras <ariolvs123@gmail.com>
2018-11-15 11:51:35 -0400

    adiconado classe aluno BO e DAO

---
+++ SispoUniron/App_Code/DAO/AlunoDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de AlunoDAO
+/// </summary>
+public class AlunoDAO
+{
+    public AlunoDAO()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+}
