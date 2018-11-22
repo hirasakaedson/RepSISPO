213df5639bc5066f358fc7cfd5e13c708bb85e60
Ariel Veras <ariolvs123@gmail.com>
2018-11-21 21:04:35 -0400

    Adicionar arquivos de projeto.

---
+++ SiteTeste/App_Code/DAO/AutorDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+
+public class AutorDAO
+{
+    Database banco = Database.Open("ConexaoBanco");
+    public int Gravar(Autor obj)
+    {
+        int sucesso;
+        if (obj.AutorID == 0)
+        {
+            var sql = "Insert Into Autores(nome,sexo)values(@0,@1)";
+            sucesso = banco.Execute(sql, obj.Nome, obj.Sexo);
+        }
+        else
+        {
+            var sql = "Update Autores Set nome=@0, sexo=@1 Where autorID=@2";
+            sucesso = banco.Execute(sql, obj.Nome, obj.Sexo, obj.AutorID);
+        }
+
+        banco.Close();
+        return sucesso;
+    }
+
+
+    public IList<Autor> ListaAutores()
+    {
+        IList<Autor> lista = new List<Autor>();
+        var sql = "Select * From Autores";
+        var resultado = banco.Query(sql);
+        if (resultado.Count() > 0)
+        {
+            Autor objAutor;
+            foreach (var item in resultado)
+            {
+                objAutor = new Autor
+                {
+                    AutorID = item.autorID,
+                    Nome = item.nome,
+                    Sexo = Convert.ToChar(item.sexo),
+                    
+                };
+                lista.Add(objAutor);
+            }
+            banco.Close();
+        }
+        else
+        {
+            banco.Close();
+            return null;
+        }
+        return lista;
+    }
+
+    public Autor Buscar(int autorID)
+    {
+        var sql = "Select * From Autores Where autorID = @0";
+        var resultado = banco.QuerySingle(sql, autorID);
+       Autor objAutor = new Autor
+        {
+            AutorID = resultado.autorID,
+            Nome = resultado.nome,
+            Sexo = resultado.sexo,
+           
+        };
+        banco.Close();
+        return objAutor;
+    }
+    public void Excluir(int autorID)
+    {
+        var sql = "Delete from Autores where autorID=@0";
+        banco.Execute(sql, autorID);
+        banco.Close();
+    }
+
+    //DAO acesso aos dados
+
+
+}
---
+++ SiteTeste/App_Code/DAO/EditoraDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+
+public class EditoraDAO
+{
+    Database banco = Database.Open("ConexaoBanco");
+    public int Gravar(Editora obj)
+    {
+        int sucesso;
+        if (obj.EditoraID == 0)
+        {
+            var sql = "Insert Into Editoras(nome,local)values(@0,@1)";
+            sucesso = banco.Execute(sql, obj.Nome, obj.Local);
+        }
+        else
+        {
+            var sql = "Update Editoras Set nome=@0, local=@1 Where editoraID=@2";
+            sucesso = banco.Execute(sql, obj.Nome, obj.Local, obj.EditoraID);
+        }
+
+        banco.Close();
+        return sucesso;
+    }
+
+
+    public IList<Editora> ListaEditoras()
+    {
+        IList<Editora> lista = new List<Editora>();
+        var sql = "Select * From Editoras";
+        var resultado = banco.Query(sql);
+        if (resultado.Count() > 0)
+        {
+            Editora objEditora;
+            foreach (var item in resultado)
+            {
+                objEditora = new Editora
+                {
+                    EditoraID = item.editoraID,
+                    Nome = item.nome,
+                    Local = item.local,
+                   
+                };
+                lista.Add(objEditora);
+            }
+            banco.Close();
+        }
+        else
+        {
+            banco.Close();
+            return null;
+        }
+        return lista;
+    }
+
+    public Editora Buscar(int alunoID)
+    {
+        var sql = "Select * From Editoras Where editoraID = @0";
+        var resultado = banco.QuerySingle(sql, alunoID);
+        Editora objEditora = new Editora
+        {
+            EditoraID = resultado.editoraID,
+            Nome = resultado.nome,
+            Local = resultado.local,
+            
+        };
+        banco.Close();
+        return objEditora;
+    }
+    public void Excluir(int editoraID)
+    {
+        var sql = "Delete from Editoras where editoraID=@0";
+        banco.Execute(sql, editoraID);
+        banco.Close();
+    }
+
+    //DAO acesso aos dados
+
+
+}
